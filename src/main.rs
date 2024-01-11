b5424871e5d3550ec008c39c181d0546b2ac5128
Andre Soares <andregsilv@gmail.com>
2024-01-11 01:45:39 -0300

    use iterator

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use sdl2::keyboard::Keycode;
 use sdl2::libc::printf;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
-use sdl2::render::TextureCreator;
+use sdl2::render::{Texture, TextureCreator};
 use sdl2::ttf;
 use std::time::Duration;
 
@@ -40,38 +40,49 @@ pub fn main() -> Result<(), String> {
 
     let lines = ["First line", "And the other line"];
     let (canvas_width, canvas_height) = canvas.output_size()?;
-
     let total_text_height = (font.recommended_line_spacing() * lines.len() as i32) as f32;
     let offset = font.recommended_line_spacing() as f32 * 4.0 / 7.0;
 
-    for (index, line) in lines.iter().enumerate() {
-        let text_surface = font
-            .render(line)
-            .blended(Color::RGBA(50, 50, 100, 255))
-            .map_err(|e| e.to_string())?;
-        let text_rect = text_surface.rect();
-        let text_texture = texture_creator
-            .create_texture_from_surface(text_surface)
-            .map_err(|e| e.to_string())?;
+    let rendered_lines: Result<Vec<(Texture<'_>, Rect, Rect)>, String> = lines
+        .iter()
+        .enumerate()
+        .map(
+            |(index, line)| -> Result<(Texture<'_>, Rect, Rect), String> {
+                let text_surface = font
+                    .render(line)
+                    .blended(Color::RGBA(50, 50, 100, 255))
+                    .map_err(|e| e.to_string())?;
+                let text_rect = text_surface.rect();
+                let text_texture = texture_creator
+                    .create_texture_from_surface(text_surface)
+                    .map_err(|e| e.to_string())?;
 
-        let center_x = canvas_width as f32 / 2.0;
-        let center_y = canvas_height as f32 / 2.0;
-        let line_center_y = center_y - text_rect.h as f32 / 2.0
-            + (font.recommended_line_spacing() * index as i32) as f32
-            - total_text_height / 2.0
-            + offset;
+                let center_x = canvas_width as f32 / 2.0;
+                let center_y = canvas_height as f32 / 2.0;
+                let line_center_y = center_y - text_rect.h as f32 / 2.0
+                    + (font.recommended_line_spacing() * index as i32) as f32
+                    - total_text_height / 2.0
+                    + offset;
 
-        let target_rect = Rect::new(
-            (center_x - (text_rect.width() as f32) / 2.0) as i32,
-            line_center_y as i32,
-            text_rect.width(),
-            text_rect.height(),
-        );
+                let target_rect = Rect::new(
+                    (center_x - (text_rect.width() as f32) / 2.0) as i32,
+                    line_center_y as i32,
+                    text_rect.width(),
+                    text_rect.height(),
+                );
 
+                Ok((text_texture, text_rect, target_rect))
+            },
+        )
+        .collect();
+    
+    for line in rendered_lines? {
+        let (text_texture, text_rect, target_rect) = line;
         canvas
             .copy(&text_texture, text_rect, target_rect)
             .map_err(|e| e.to_string())?;
     }
+    
 
     canvas.present();
 
